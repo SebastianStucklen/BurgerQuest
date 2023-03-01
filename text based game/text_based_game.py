dae9c9073a71696286ad4e949c805dd24a0a23a6
817677 <817677@605-110-S02>
2023-03-01 14:39:34 -0700

    Add project files.

---
+++ text based game/text_based_game.py
@@ -0,0 +1,30 @@
+import random
+import time
+room = 1
+choice = "PLACEHOLDER"
+print("welcome to")
+time.sleep(1)
+print("cool")
+time.sleep(0.8)
+print("dungeon")
+time.sleep(0.8)
+print("GUY")
+time.sleep(2)
+print("the greatest game since mort the chicken on the ps2")
+time.sleep(1)
+print("Type help for help")
+inventory = []
+hasShield = False
+sword = 7 #change this to 15 when player gets upgraded sword
+hasPotion = False
+def bossFight(name,health,attack):
+    #put stuff here later
+    print("placeholder")
+def help():
+    #put stuff here
+    print("Type I to view inventory")
+while choice != "quit":
+    if room == 1:
+        choice = input("You are in a grand hall. There are four doors. north, east, south, and west.")
+        if choice == 'help':
+            help()
