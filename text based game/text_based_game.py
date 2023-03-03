d6a988788cbfa5ee66ffc5e040f185ad2be8ed18
Seb Stuck <sebas7236@icloud.com>
2023-03-03 14:03:55 -0700

    3.3.2023 203 pm

--- text based game/text_based_game.py
+++ text based game/text_based_game.py
@@ -1,9 +1,9 @@
 import random
 import time
+global playerHealth, hasShield, sword, hasPotion, hasArmor, defending, bossIsAttacking
 #other things
-room = 1
+room = 10
 choice = "PLACEHOLDER"
-dead = False
 bossIsAttacking = False
 #intro
 def intro():
@@ -29,9 +29,10 @@ sword = 7
 hasPotion = True
 hasArmor = True
 defending = False
+#boss fight
 def bossFight(bossHealth,damage):
 	global playerHealth, hasShield, sword, hasPotion, hasArmor, defending, bossIsAttacking
-	while bossHealth > 0 or playerHealth > 0:
+	while bossHealth >= 0 or playerHealth >= 0:
 		print("Boss health:")
 		print(bossHealth)
 		print("Your health")
@@ -70,18 +71,20 @@ def bossFight(bossHealth,damage):
 			if defending == True:
 				playerHealth-=damage/2
 			bossIsAttacking = False
+		#return playerHealth and bossHealth
 def northBoss():
 	print("test")
 	bossFight(100,10)
+#help
 def help():
     #put stuff here
     print("Type I to view inventory")
-while choice != "quit" or dead != True:
-	if playerHealth <= 0:
-		dead = True
+while choice != "quit" or playerHealth > 0:
 	if room == 1:
 		choice = input("You are in a grand hall. There are four doors. north, east, south, and west.")
 		if choice == 'help':
 			help()
-	bossFight(100,10)
+	#playerHealth = -100
+	#bossFight(100,34)
+
 print("you dead")
